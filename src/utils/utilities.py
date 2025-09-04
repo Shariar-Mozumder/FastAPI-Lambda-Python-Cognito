5825e80c93a02890890c36173451182802f7a30a
Md. Shariar Hossain <26835546+Shariar-Mozumder@users.noreply.github.com>
2025-09-05 02:17:31 +0600

    Update utilities.py

--- src/utils/utilities.py
+++ src/utils/utilities.py
@@ -8,8 +8,10 @@ def object_to_dict(obj):
     for key, value in values.items():
         if isinstance(value, datetime.datetime):
             values[key] = value.isoformat()
+    api_response("User signed up","successfully executed",500,200, "User signed up successfully")
     # Remove attributes to ignore
     ignore_attributes = ['cls']  # Add any other attributes to ignore
     for attr in ignore_attributes:
         values.pop(attr, None)
     return values
+    api_response("User signed up","successfully executed",500,200, "User signed up successfully")
