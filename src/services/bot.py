3f26d4fcf0627f4be3eaba4e8051c6cd5d208708
Md. Shariar Hossain <26835546+Shariar-Mozumder@users.noreply.github.com>
2025-09-05 03:44:59 +0600

    Update bot.py

--- src/services/bot.py
+++ src/services/bot.py
@@ -17,7 +17,7 @@ from langchain_core.runnables import RunnableMap
 # Your Hugging Face API key (if using Hugging Face Hub)
 # huggingface_api_key = ""  # Replace with your actual Hugging Face Hub API key
 huggingface_api_key = ""  # Replace with your actual Hugging Face Hub API key
-
+print("test")
 
 def generate_pet_name(animal_type, pet_color):
     # Create the prompt template
